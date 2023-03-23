09a9883316444f8560aa652c10e2aa0752a9cc8f
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-23 11:13:14 -0300

    add primeiro.go

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,12 @@
+// programas executaveis iniciam pelo packge main
+package main
+
+/* os codigos em go sao
+organizados em packge e para usar vc precisar importar o packger*/
+import "fmt"
+
+// funcao de entrada de todo programa go
+func main() {
+	fmt.Print("primeiro ")
+	fmt.Println(" programa ")
+}
