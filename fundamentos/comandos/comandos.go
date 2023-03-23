4e0bbe956d4832cbce8f2f4c6b171d1dddf8c0e6
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-23 12:56:41 -0300

    add comandos.go

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("outro programa em  go , %s !", "deu certo passar o argumento")
+}
+
+/*
+go vet + nome do arquivo.go mostra os erros
+
+go build + nome do arquivo.go gera o executavel do arquivo
+
+go run nome do arquivo.go ja copila e executa o programa
+
+
+o caminho é go/pkp/mod/github.com
+
+go install github.com/go-sql-driver/mysql@latest
+
+
+*/
