db8adfe11a314d03d8fe23ef061d4eee0cce5b9e
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-24 10:36:48 -0300

    add aritimeticos.go

---
+++ fundamentos/aritimeticos/aritimeticos.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 8
+	b := 5
+	fmt.Println("soma :", a+b)
+	fmt.Println("subtracao :", a-b)
+	fmt.Println("multiplicacao :", a*b)
+	fmt.Println("divisao :", a/b)
+	fmt.Println("modulo :", a%b)
+}
