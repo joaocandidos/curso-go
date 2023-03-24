9912bbb0d13dc00269833241258c81a5749cac43
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-24 14:50:14 -0300

    add relacionais.go

---
+++ fundamentos/relacionais/relacionais.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("sao iguais:", "banana" == "banana")
+	fmt.Println("sao diferentes:", 3 != 2)
+	fmt.Println("menor que:", 3 < 2)
+	fmt.Println("menor ou  igual:", 3 <= 3)
+}
