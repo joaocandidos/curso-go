84083c7253f0062fae6869ceaf555ee38af5fa03
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-27 14:30:49 -0300

    add new file arrayinterno.go e modifiquei slicemake.go

--- array-slices-map/slicemake/slicemake.go
+++ array-slices-map/slicemake/slicemake.go
@@ -10,5 +10,7 @@ func main() {
 	fmt.Println(s)
 	//cria um slace de 10 posicoes a partir de um array de 20 posicoes
 	s = make([]int, 10, 20)
+	//len(S) verifica o tamanho do slice s
+	//cap(S) verifica o tamanho do array interno a partir do qual o slice é formado
 	fmt.Println(s, len(s), cap(s))
 }
